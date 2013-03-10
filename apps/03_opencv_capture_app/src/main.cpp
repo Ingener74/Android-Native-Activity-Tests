0491cc88b0914e642a36fea0ee3edfab5cacdc7f
Pavel Shnayder <pavel_shnayder@3divi.com>
2013-03-10 19:45:06 +0600

    added opencv capture application

---
+++ apps/03_opencv_capture_app/src/main.cpp
@@ -0,0 +1,19 @@
+
+#include <android_native_app_glue.h>
+#include <tools.h>
+
+const char* caption =
+		"Android Native-Activity OpenCV capture application"
+		;
+
+void android_main( android_app* app ){
+
+	LOGI("android main", caption);
+
+//	IGraphicsService* gs = new OpenGLES10GraphicsService();
+
+//	OpenGLES10EventHandler* eh = new OpenGLES10EventHandler(app, gs);
+
+//	if(eh)
+//		eh->run();
+}
