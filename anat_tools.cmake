337e0dc1097728479b0febb49aad5e45961090e9
Ingener74 <shnaiderpasha@gmail.com>
2013-03-31 08:18:49 +0600

    added glm library

--- anat_tools.cmake
+++ anat_tools.cmake
@@ -1,6 +1,11 @@
 
 set(REPO_HOME ${CMAKE_CURRENT_LIST_DIR})
 
+set(ANAT_BUILD_DIR ${CMAKE_BINARY_DIR})
+
+set(ANAT_INSTALL ${REPO_HOME}/${CMAKE_SYSTEM_NAME}-build)
+set(ANAT_INSTALL_3RDPARTY ${ANAT_INSTALL}/3rdparty)
+
 set(ANDROID_OPENCV_DIR
     ${REPO_HOME}/3rdparty/OpenCV-2.4.4-android-sdk/sdk/native
     )
@@ -23,3 +28,15 @@ set(ANAT_LINK
     )
 
 set(ANAT_NATIVE_APP_GLUE ${ANDROID_NATIVE_APP_GLUE_DIR}/android_native_app_glue.c)
+
+macro(print_information)
+    message("REPO_HOME                    = ${REPO_HOME}")
+    message("ANDROID_OPENCV_DIR           = ${ANDROID_OPENCV_DIR}")
+    message("ANDROID_NDK                  = ${ANDROID_NDK}")
+    message("ANDROID_NATIVE_APP_GLUE_DIR  = ${ANDROID_NATIVE_APP_GLUE_DIR}")
+    message("ANAT_INCLUDE                 = ${ANAT_INCLUDE}")
+    message("ANAT_LINK                    = ${ANAT_LINK}")
+    message("ANAT_BUILD_DIR               = ${ANAT_BUILD_DIR}")
+    message("ANAT_INSTALL                 = ${ANAT_INSTALL}")
+    message("ANAT_INSTALL_3RDPARTY        = ${ANAT_INSTALL_3RDPARTY}")
+endmacro(print_information)
