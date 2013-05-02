7160c652c792212322bc5dc8383670bb766223f4
Pavel Shnayder <pavel_shnayder@3divi.com>
2013-05-02 19:50:08 +0600

    added ois to 3rdparty for Linux chainge in ogre_test_001

--- apps/ogre_test_001/src/main.cpp
+++ apps/ogre_test_001/src/main.cpp
@@ -1,20 +1,69 @@
 
 #include <iostream>
-#include <OGRE/Ogre.h> 
+#include <OGRE/Ogre.h>
 
-const char* caption = 
-	"OGRE Test application"
-	;
+#include <OIS/OIS.h>
+
+const char* caption =
+		"OGRE Test application"
+		;
+const char* usage =
+		"Usage: ./ogre_test_001 <path-to-resources.cfg>"
+		;
 using namespace Ogre;
 
 int main( int argc, char* argv[] ){
-	std::cout << caption << std::endl;
-	Root* root = new Root;
 
-	if(root){
+	if(argc == 2){
+		std::cout << caption << std::endl;
+		Root* root = new Root;
+
+		ConfigFile cf;
+		cf.load(argv[1]);
+
+		ConfigFile::SectionIterator seci = cf.getSectionIterator();
+
+		String secName, typeName, archName;
+
+		while(seci.hasMoreElements()){
+			secName = seci.peekNextKey();
+
+			ConfigFile::SettingsMultiMap *settings = seci.getNext();
+			ConfigFile::SettingsMultiMap::iterator i;
+			for( i = settings->begin(); i != settings->end(); ++i ){
+				typeName = i->first;
+				archName = i->second;
+				ResourceGroupManager::getSingleton().addResourceLocation(archName, typeName, secName);
+			}
+		}
+
+		if(root){
+			if(root->showConfigDialog()){
+				RenderWindow* window = root->initialise(true, "ogre test 001");
+
+				ResourceGroupManager::getSingleton().initialiseAllResourceGroups();
+
+				SceneManager* sceneMgr = root->createSceneManager(ST_GENERIC);
+
+				Camera* camera = sceneMgr->createCamera("simpleCamera");
+
+				Viewport* viewPort = window->addViewport(camera);
+
+//				OIS::ParamList pl;
+//				size_t windowHnd = 0;
+
+				root->startRendering();
+
+			}else{
+				delete root;
+				std::cerr << "error in root" << std::endl; exit(EXIT_FAILURE);
+			}
+		}else{
+			std::cout << "root is NULL" << std::endl;
+		}
+
 	}else{
-		std::cout << "root is NULL" << std::endl;
+		std::cerr << usage << std::endl; exit(EXIT_FAILURE);
 	}
-
-	return 0;
+	exit(EXIT_SUCCESS);
 }
