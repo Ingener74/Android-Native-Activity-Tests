70bc8737eb9add1176962a63ee954951dcb98307
Pavel Shnayder <pavel_shnayder@3divi.com>
2013-07-15 18:54:09 +0600

    fix

--- apps/ogre_opencv_visualizer/src/main.cpp
+++ apps/ogre_opencv_visualizer/src/main.cpp
@@ -176,8 +176,72 @@ int main( int argc, char* argv[] ){
 	/*
 	 * scene construction
 	 */
-//	Ogre::Entity* obj = _sceneManager->createEntity()
 
+	Ogre::MeshPtr mesh = Ogre::MeshManager::getSingleton().createManual("Screen", "General");
+	Ogre::SubMesh* subMesh = mesh->createSubMesh();
+
+	mesh->sharedVertexData = new Ogre::VertexData;
+	mesh->sharedVertexData->vertexCount = 6;
+
+	Ogre::VertexDeclaration* decl = mesh->sharedVertexData->vertexDeclaration;
+	size_t offset = 0;
+
+	decl->addElement(0, offset, Ogre::VET_FLOAT3, Ogre::VES_POSITION);
+	offset += Ogre::VertexElement::getTypeSize(Ogre::VET_FLOAT3);
+
+	decl->addElement(0, offset, Ogre::VET_FLOAT3, Ogre::VES_DIFFUSE);
+	offset += Ogre::VertexElement::getTypeSize(Ogre::VET_FLOAT3);
+
+	Ogre::HardwareVertexBufferSharedPtr vertexBuffer = Ogre::HardwareBufferManager::
+			getSingleton().createVertexBuffer(
+					offset,
+					mesh->sharedVertexData->vertexCount,
+					Ogre::HardwareBuffer::HBU_STATIC_WRITE_ONLY);
+
+	float* vertices = static_cast<float*>(vertexBuffer->lock(Ogre::HardwareBuffer::HBL_NORMAL));
+
+	vertices[0] = 0; vertices[1] = 0; vertices[2] = 0;
+	vertices[3] = 1; vertices[4] = 0; vertices[5] = 0;
+
+	vertices[6] = 10; vertices[7] = 0; vertices[8] = 0;
+	vertices[9] = 1; vertices[10] = 0; vertices[11] = 0;
+
+	vertices[12] = 0; vertices[13] = 10; vertices[14] = 0;
+	vertices[15] = 1; vertices[16] = 0; vertices[17] = 0;
+
+	vertices[18] = 10; vertices[19] = 10; vertices[20] = 0;
+	vertices[21] = 1; vertices[22] = 0; vertices[23] = 0;
+
+	vertexBuffer->unlock();
+
+	Ogre::HardwareIndexBufferSharedPtr* indexBuffer = Ogre::HardwareBufferManager::getSingleton().
+			createIndexBuffer(Ogre::HardwareIndexBuffer::IT_16BIT, mesh->sharedVertexData->vertexCount, Ogre::HardwareBuffer::HBU_STATIC);
+
+	uint16_t* indices = static_cast<uint16_t*>(indexBuffer->lock(Ogre::HardwareBuffer::HBL_NORMAL));
+
+	indices[0] = 0;
+	indices[1] = 1;
+	indices[2] = 2;
+
+	indices[3] = 3;
+	indices[4] = 2;
+	indices[5] = 1;
+
+	indexBuffer->unlock();
+
+	mesh->sharedVertexData->vertexBufferBinding->setBinding(0, vertexBuffer);
+	subMesh->useSharedVertices = true;
+	subMesh->indexData->indexBuffer = indexBuffer;
+	subMesh->indexData->indexCount = mesh->sharedVertexData->vertexCount;
+	subMesh->indexData->indexStart = 0;
+
+	mesh->_setBounds(Ogre::AxisAlignedBox(0, 0, 0, 10, 10, 10));
+
+	mesh->load();
+
+	Ogre::Entity* screenEntity = _sceneManager->createEntity("ScreenEntity", "Screen", "General");
+	Ogre::SceneNode* screenNode = _sceneManager->getRootSceneNode()->createChildSceneNode();
+	screenNode->attachObject(screenEntity);
 	/*
 	 * end scene construction
 	 */
